954302899b9ab0e0aaa1033cbd7fed3d75334c30
Gagandeep <iamsinghhhhgagan@gmail.com>
2024-04-23 16:12:23 +0530

    netflix-gpt project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-3xl text-green-400">
+        Hello world
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
